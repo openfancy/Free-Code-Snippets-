6b210faccd29fd56ab6da34405d7000ecd038439
RudeeeMan <fboegge@gmail.com>
2014-01-09 17:07:59 +0100

    openfancy
    internal

--- check openfancy user - internal use/edit.php
+++ check openfancy user - internal use/edit.php
@@ -1,6 +1,6 @@
 
  <span id="c1" style="font: 12px lucida grande;">   <?php
-mysql_connect("SERVER", "DEIN MYAQL USERNAME","PASSWORD FOR MYSQL") or die ("Keine Verbindung moeglich");
+mysql_connect("SERVER", "DEIN MYSQL USERNAME","PASSWORD FOR MYSQL") or die ("Keine Verbindung moeglich");
 mysql_select_db("DATENBANKNAME") or die ("Die Datenbank existiert nicht.");
 
     ?>  <b><?php
